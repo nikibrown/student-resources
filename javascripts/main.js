038b58939745537a56f20a55fb488710a1f15fac
Niki Brown <nbrown@fitbit.com>
2017-04-22 10:56:23 -0400

    add footer, comment js code

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
-    var links = $( "main a" ).length;
-    $( ".intro" ).text( links + " helpful links and counting!");
+$(document).ready(function() { // run when the document is ready
+    var links = $( "main a" ).length; // make a variable called links - look for # of links in the <main> element
+    $( "em.intro" ).text( links + " helpful links and counting!"); // find the <em> tag with a class of intro and update the text of the element with # of links
 });
